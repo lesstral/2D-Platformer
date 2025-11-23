96e43db276e1ecbf78d69613221d5d394745dabb
lesstral <lesstral@gmail.com>
2025-11-24 02:24:36 +0700

    parallax backgrounds script
    also had to tweak sorting layer on some prefabs to correctly work with new background

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -1,16 +1,65 @@
 using UnityEngine;
+using System.Collections.Generic;
+using Unity.Cinemachine;
 
-public class ParallaxBackground : MonoBehaviour
+[System.Serializable]
+public class ParallaxLayer
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public List<Transform> objects = new List<Transform>();
+    [Tooltip("Parallax multiplier, 0 = no movement, 1 = full movement with camera")]
+    public float parallaxFactor = 0.5f;
+}
+public class ParallaxBackground : MonoBehaviour
+
+{
+    [Header("Camera Settings")]
+    public Transform cameraTransform;
+    private Vector3 previousCameraPosition;
+
+    [Header("Layers")]
+    public ParallaxLayer sky = new ParallaxLayer { parallaxFactor = 0.1f };
+    public ParallaxLayer clouds = new ParallaxLayer { parallaxFactor = 0.3f };
+    public ParallaxLayer background = new ParallaxLayer { parallaxFactor = 0.5f };
+    public ParallaxLayer objects = new ParallaxLayer { parallaxFactor = 0.6f };
+    public ParallaxLayer foreground = new ParallaxLayer { parallaxFactor = 0.8f };
+
+    [Header("Optional")]
+    public bool verticalParallax = false;
+
     void Start()
     {
-        
+        if (cameraTransform == null)
+        {
+            cameraTransform = Camera.main.transform;
+        }
+        previousCameraPosition = cameraTransform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        Vector3 deltaMovement = cameraTransform.position - previousCameraPosition;
+
+        ApplyParallax(sky, deltaMovement);
+        ApplyParallax(clouds, deltaMovement);
+        ApplyParallax(background, deltaMovement);
+        ApplyParallax(objects, deltaMovement);
+        ApplyParallax(foreground, deltaMovement);
+
+        previousCameraPosition = cameraTransform.position;
+    }
+
+    void ApplyParallax(ParallaxLayer layer, Vector3 deltaMovement)
+    {
+        if (layer.objects == null) return;
+
+        foreach (Transform obj in layer.objects)
+        {
+            if (obj == null) continue;
+
+            Vector3 movement = new Vector3(deltaMovement.x * layer.parallaxFactor,
+                                           verticalParallax ? deltaMovement.y * layer.parallaxFactor : 0,
+                                           0);
+            obj.position += movement;
+        }
     }
 }
