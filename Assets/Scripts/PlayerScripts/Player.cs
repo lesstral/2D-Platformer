cbe72826f69398a84621c1639f3c1eb77ba0c53b
lesstral <lesstral@gmail.com>
2025-12-14 02:19:02 +0700

    Update Player.cs

--- Assets/Scripts/PlayerScripts/Player.cs
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -12,6 +12,15 @@ public class Player : MonoBehaviour, IControllable
     [SerializeField] LayerMask _groundLayer;
     private PlayerState _playerState = PlayerState.Idle;
 
+    private void OnEnable()
+    {
+        Events.InGameEvents.onFlagReached.Subscribe(OnVictory);
+    }
+    private void OnDisable()
+    {
+        Events.InGameEvents.onFlagReached.Unsubscribe(OnVictory);
+    }
+
     public void Move(Vector2 direction)
     {
 
@@ -19,7 +28,7 @@ public class Player : MonoBehaviour, IControllable
         _rigidbody2D.linearVelocity = new Vector2(velocity.x, _rigidbody2D.linearVelocityY);
         HandleStates();
     }
-    public void HandleStates()
+    private void HandleStates()
     {
         if (_rigidbody2D.linearVelocity.x > 0)
         {
@@ -35,22 +44,29 @@ public class Player : MonoBehaviour, IControllable
         }
         Events.PlayerEvents.onPlayerStateChanged.Publish(_playerState);
     }
-    public void Jump()
+
+    private void Jump()
     {
-        Debug.Log("Jump" + IsGrounded());
+
         if (IsGrounded())
         {
             _rigidbody2D.linearVelocity = new Vector2(_rigidbody2D.linearVelocityX, _jumpForce);
             Events.PlayerEvents.onPlayerActionPerformed.Publish(PlayerAction.Jump);
         }
     }
+    private void OnVictory()
+    {
+        gameObject.SetActive(false);
+    }
     private bool IsGrounded()
     {
+#if UNITY_EDITOR
         Debug.DrawRay(_groundCheck.position, transform.forward * 5f, Color.red);
-
+#endif
         return Physics2D.OverlapBox(_groundCheck.position, new Vector2(0.15f, 0.1f), 0f, _groundLayer);
 
     }
+
     public void Die()
     {
         Events.PlayerEvents.onPlayerActionPerformed.Publish(PlayerAction.Death);
