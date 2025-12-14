179cb696d667105c4216934add54a2c418fe2a06
lesstral <lesstral@gmail.com>
2025-12-14 21:24:15 +0700

    Update LevelSoundPlayer

--- Assets/Scripts/GameplayScripts/LevelSoundPlayer.cs
+++ Assets/Scripts/GameplayScripts/LevelSoundPlayer.cs
@@ -5,20 +5,30 @@ public class LevelSoundPlayer : MonoBehaviour
     [SerializeField] AudioSource _audioSource;
     [SerializeField] AudioClip _respawnProcessClip;
     [SerializeField] AudioClip _victoryClip;
+    [SerializeField] AudioClip _gameoverClip;
+
 
     private void OnEnable()
     {
         Events.InGameEvents.onPlayerRespawnStarted.Subscribe(PlayRespawnClip);
         Events.InGameEvents.onPlayerRespawnEnded.Subscribe(Stop);
+        Events.UIEvents.onVictory.Subscribe(PlayVictoryClip);
+        Events.UIEvents.onGameOver.Subscribe(PlayGameoverClip);
     }
     private void OnDisable()
     {
         Events.InGameEvents.onPlayerRespawnStarted.Unsubscribe(PlayRespawnClip);
         Events.InGameEvents.onPlayerRespawnEnded.Unsubscribe(Stop);
+        Events.UIEvents.onVictory.Unsubscribe(PlayVictoryClip);
+        Events.UIEvents.onGameOver.Unsubscribe(PlayGameoverClip);
     }
-    private void PlayRespawnClip()
+    private void PlayVictoryClip(int score) => PlayClip(_victoryClip);
+    private void PlayGameoverClip() => PlayClip(_gameoverClip);
+    private void PlayRespawnClip() => PlayClip(_respawnProcessClip);
+    private void PlayClip(AudioClip clip)
     {
-        _audioSource.clip = _respawnProcessClip;
+        Stop();
+        _audioSource.clip = clip;
         _audioSource.Play();
     }
     private void Stop()
